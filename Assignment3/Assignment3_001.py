2a552121396083bbe8a5abc693cf6b07c927d5ff
hugohjerten <hugohjerten@gmail.com>
2015-04-20 16:35:36 +0200

    First Task finnished in Assignment #1

--- Assignment3/Assignment3_001.py
+++ Assignment3/Assignment3_001.py
@@ -46,34 +46,71 @@ def interpolationLagrange(x, xm, ym):
 	p = array([lagrange(x, i, xm) for i in range(n+1)])
 	y = dot(ym, p)
 	#print y
-	print 'The xsize value for Lagrange:'
-	print len(x)
-	print 'The ysizevalue for Lagrange:'
-	print len(y)
 	return y
+
+def interpolationVander(x, y):
+	V = vander(x,len(x))
+	a = solve(V,y)
+	return a
 
 x1 = linspace(0.9,5.1,1000)
 polyfitterReturn =  polyfitter(days, energy, 'Polyfitter')
 interpolationLagrangeReturn = interpolationLagrange(x1, days, energy)
+interpolationVanderReturn = interpolationVander(days, energy)
 
-print polyfitterReturn
-print interpolationLagrangeReturn
+polyFitEnergy0 = polyval(polyfitterReturn, 8.0)
+lagrangeEnergy0 = interpolationLagrange(8.0, days, energy)
+vanderEnergy0 = polyval(interpolationVanderReturn, 8.0)
 
-subplot(211)
+print 'Polyfit value: {}'.format(polyFitEnergy0)
+print 'Lagrange value: {}'.format(lagrangeEnergy0)
+print 'Vandermonde value: {}'.format(vanderEnergy0)
+
+figure(1)
+subplot(311)
 plot(x1, polyval(polyfitterReturn, x1))
 grid(True)
 title('Polyfitter')
-subplot(212)
+subplot(312)
 plot(x1, interpolationLagrangeReturn)
 grid(True)
 title('Lagrange')
+subplot(313)
+plot(x1, polyval(interpolationVanderReturn, x1))
+grid(True)
+title('Vandermonde')
 show()
+for i in range(len(energy)):
+	dx = 0.01
+	energy[i] += dx
+	polyfitterReturn =  polyfitter(days, energy, 'Polyfitter')
+	interpolationLagrangeReturn = interpolationLagrange(x1, days, energy)
+	interpolationVanderReturn = interpolationVander(days, energy)
+	
+	polyFitEnergy = polyval(polyfitterReturn, 8.0)
+	lagrangeEnergy = interpolationLagrange(8.0, days, energy)
+	vanderEnergy = polyval(interpolationVanderReturn, 8.0)
+	dYPoly = abs(polyFitEnergy0 - polyFitEnergy)
+	dYLagrange = abs(lagrangeEnergy0 - lagrangeEnergy)
+	dYVander = abs(vanderEnergy0 - vanderEnergy)
+	relPol = (dYPoly/max(energy)) / (dx/max(days))
+	relLag = (dYLagrange/max(energy)) / (dx/max(days))
+	relVan = (dYVander/max(energy)) / (dx/max(days))
+	print 'Change the data point #{}, the relative sensitivty of polyfitter: {}'.format(i+1, relPol)
+	print 'Change the data point #{}, the relative sensitivty of lagrange: {}'.format(i+1, relLag)	
+	print 'Change the data point #{}, the relative sensitivty of vander: {}'.format(i+1, relVan)
 
-##Göra sista delen på Task 1##
 
 
+## Task 4 ##
+
+def function(x):
+	y = 1.0 / (1.0 + 25*x**2)
+	return y
 
 
+n = 3
+x2 = linspace(-1,1,n)
 
 
 
