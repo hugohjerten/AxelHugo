594b41cf19b63b2e2c095f28118bdb126aad65e3
Sebastianelm <elmsebb@hotmail.com>
2015-04-27 13:14:19 +0200

    Started assignment 4 task 1

---
+++ Assignment4/task1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 27 10:09:23 2015
+@author: sebastianelm
+"""
+from scipy import *
+from matplotlib.pylab import *
+from __future__ import division # 2st. Understreck på varsin sida
+
+def cubespline(xint,yint):
+    
