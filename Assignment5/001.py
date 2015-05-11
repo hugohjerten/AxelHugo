443813f99edb2ac603e72f128f1650dbd110474d
hugohjerten <hugohjerten@gmail.com>
2015-05-11 18:49:27 +0200

    Update.

--- Assignment5/001.py
+++ Assignment5/001.py
@@ -6,12 +6,21 @@ from numpy import *
 
 def simpsons(f, a, b, n):
 	h = abs(b-a)/n
+	tmp = 0
 	for j in range(1,n):
-		
+		xj = a + j*h
+		tmp += 2*f(xj) + 4*f(xj + h/2)
+	integral = (h/6)*(f(a) + 4*f(a + h/2) + f(b) + tmp)
 	
 	
-	
-	
-	return
+	return integral 
 
+def 3pointGauss(f, a, b, n):
+	h = abs(b-a)/n
+	tmp = 0
+	for j in range(0,n):
+		xj = a + j*h
+		tmp1 = f(xj + h*(5 + math.sqrt(15))/10)
+		
+	return
 
